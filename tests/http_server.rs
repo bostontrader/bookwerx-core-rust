acb99777804157c937fea8caf8610fb4bd3398f4
Thomas Radloff <bostontrader@gmail.com>
2019-07-01 09:28:42 +0800

    More random and futile tweaking to get the Travis tests working, even though the local tests work just fine.

--- tests/http_server.rs
+++ tests/http_server.rs
@@ -1,19 +1,14 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 #[macro_use] extern crate rocket;
 
-use bookwerx_core_rust::constants as C;
 use bookwerx_core_rust::routes as R;
-
-use assert_cmd::prelude::*;
-use predicates::prelude::*;
-use std::process::Command;
 
 //use super::rocket;
 use rocket::local::Client;
 use rocket::http::Status;
 
 #[test]
-fn get_index() -> Result<(), Box<std::error::Error>> {
+fn get_index() -> Result<(), Box<dyn std::error::Error>> {
 
     let rocket = rocket::ignite().mount("/", routes![R::index]);
     let client = Client::new(rocket).expect("valid rocket instance");
@@ -25,7 +20,7 @@ fn get_index() -> Result<(), Box<std::error::Error>> {
 }
 
 #[test]
-fn get_accounts() -> Result<(), Box<std::error::Error>> {
+fn get_accounts() -> Result<(), Box<dyn std::error::Error>> {
 
     let rocket = rocket::ignite().mount("/", routes![R::get_accounts]);
     let client = Client::new(rocket).expect("valid rocket instance");
@@ -37,7 +32,7 @@ fn get_accounts() -> Result<(), Box<std::error::Error>> {
 }
 
 #[test]
-fn post_account() -> Result<(), Box<std::error::Error>> {
+fn post_account() -> Result<(), Box<dyn std::error::Error>> {
 
     let rocket = rocket::ignite().mount("/", routes![R::post_account]);
     let client = Client::new(rocket).expect("valid rocket instance");
