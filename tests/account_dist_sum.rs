ca07251560d32c4c1f91e8b2baf0d9e14b4a2a8d
Thomas Radloff <bostontrader@gmail.com>
2020-06-21 15:59:27 +0800

    Expand test code coverage.

--- tests/account_dist_sum.rs
+++ tests/account_dist_sum.rs
@@ -16,27 +16,27 @@ pub fn account_dist_sum(client: &Client, apikey: &String, accounts: &Vec<D::Acco
     response = client.get(format!("/account_dist_sum?apikey={}&account_id={}", &apikey, (accounts.get(0).unwrap()).id)).dispatch();
     r = serde_json::from_str(&(response.body_string().unwrap())[..]).unwrap();
     assert_eq!(response.status(), Status::Ok);
-    //assert_eq!(r.sum.amount, 0);
-    //assert_eq!(r.sum.exp, 0);
+    assert_eq!(r.sum.amount, 12);
+    assert_eq!(r.sum.exp, 0);
 
     // 2.2 time_start
-    response = client.get(format!("/account_dist_sum?apikey={}&account_id={}&time_start=2020", &apikey, (accounts.get(0).unwrap()).id)).dispatch();
+    response = client.get(format!("/account_dist_sum?apikey={}&account_id={}&time_start=2020-12", &apikey, (accounts.get(0).unwrap()).id)).dispatch();
     r = serde_json::from_str(&(response.body_string().unwrap())[..]).unwrap();
     assert_eq!(response.status(), Status::Ok);
-    //assert_eq!(r.sum.amount, 0);
-    //assert_eq!(r.sum.exp, 0);
+    assert_eq!(r.sum.amount, 9);
+    assert_eq!(r.sum.exp, 0);
 
     // 2.3 time_stop
-    response = client.get(format!("/account_dist_sum?apikey={}&account_id={}&time_stop=2020", &apikey, (accounts.get(0).unwrap()).id)).dispatch();
+    response = client.get(format!("/account_dist_sum?apikey={}&account_id={}&time_stop=2020-12", &apikey, (accounts.get(0).unwrap()).id)).dispatch();
     r = serde_json::from_str(&(response.body_string().unwrap())[..]).unwrap();
     assert_eq!(response.status(), Status::Ok);
-    //assert_eq!(r.sum.amount, 0);
-    //assert_eq!(r.sum.exp, 0);
+    assert_eq!(r.sum.amount, 7);
+    assert_eq!(r.sum.exp, 0);
 
     // 2.4 time_start and time_stop
-    response = client.get(format!("/account_dist_sum?apikey={}&account_id={}&time_start=2020&time_stop=2021", &apikey, (accounts.get(0).unwrap()).id)).dispatch();
+    response = client.get(format!("/account_dist_sum?apikey={}&account_id={}&time_start=2020-12&time_stop=2020-12", &apikey, (accounts.get(0).unwrap()).id)).dispatch();
     r = serde_json::from_str(&(response.body_string().unwrap())[..]).unwrap();
     assert_eq!(response.status(), Status::Ok);
-    //assert_eq!(r.sum.amount, 0);
-    //assert_eq!(r.sum.exp, 0);
+    assert_eq!(r.sum.amount, 4);
+    assert_eq!(r.sum.exp, 0);
 }
