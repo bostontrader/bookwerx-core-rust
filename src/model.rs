5a796e35b7d160266be34c5bff385fa4f3736d09
Christian Legnitto <christian.legnitto@robinhood.com>
2020-09-02 21:09:34 -1000

    This now compiles.
    Two things:
    
    1. updated to Juniper master. While not strictly necessary, we are doing a release soon
    so this is what it will need to be. It also lets you use rocket async if you want.
    
    2. I have never used rocket so I have no clue if this is right...but it compiles at least. I don't have a local db for testing, sorry.

--- src/model.rs
+++ src/model.rs
@@ -1,16 +1,10 @@
-use crate::db::{MyRocketSQLConn};
+use crate::db::MyRocketSQLConn;
 use std::sync::{Arc, Mutex};
 
-
-pub struct JunDatabase {
-    pub conn: Arc<Mutex<Option<MyRocketSQLConn>>>,
-    //pub conn: Arc<Mutex<Option<Box<MyRocketSQLConn>>>>,
-}
+pub struct JunDatabase(pub Arc<Mutex<Option<MyRocketSQLConn>>>);
 
 impl JunDatabase {
     pub fn new() -> JunDatabase {
-        JunDatabase {
-            conn: Arc::new(Mutex::new(None)),
-        }
+        JunDatabase(Arc::new(Mutex::new(None)))
     }
 }
--- src/schema.rs
+++ src/schema.rs
@@ -2,7 +2,7 @@ use crate::model::{JunDatabase};
 
 pub struct Query;
 
-#[juniper::object(
+#[juniper::graphql_object(
     Context = JunDatabase
     Scalar = juniper::DefaultScalarValue,
 )]
@@ -11,23 +11,18 @@ pub struct Query;
 impl Query {
 
     // curl -X POST -H "Content-Type: application/json" -d '{"query": "{ hello }"}' http://localhost:3003/graphql
-    fn hello() -> i32 {
+    fn hello(&self) -> i32 {
         42
     }
 
-    fn currencies(database: &JunDatabase) -> i32 {
+    fn currencies(&self, database: &JunDatabase) -> i32 {
 
         let mut params= Vec::new();
         params.push("catfood".to_string());
 
-        let m = &*database.conn;
-        let mut m1 = m.lock().unwrap();
-        //let m2 = m1.as_mut();
-        let m2 = m1.as_ref();
-        let mut m3 = m2.unwrap();
-        //(*m3).get_conn();
-        //let m1 = m.lock().unwrap().as_ref().unwrap();
-        //let m4 = m3.prep_exec("SELECT id, apikey, rarity, symbol, title from currencies where apikey = :apikey", params);
+        let mut m = database.0.lock().unwrap();
+        let m = m.as_deref_mut().unwrap();
+        let _ = m.prep_exec("SELECT id, apikey, rarity, symbol, title from currencies where apikey = :apikey", params);
 
 
         42
